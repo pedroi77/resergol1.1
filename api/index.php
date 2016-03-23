9c0d5c37bf6b789cc01a788529c648d383f10d38
pedroi77 <iralapedro@gmail.com>
2016-03-23 14:39:49 -0300

    Crear duenios en proceso...

--- api/index.php
+++ api/index.php
@@ -3,6 +3,7 @@
 ini_set('display_errors', 'On');
 error_reporting(E_ALL | E_STRICT);
 require_once("modelos/tiposDocs.php");
+require_once("modelos/duenios.php");
 require_once("util/jsonResponse.php");
 require 'Slim/Slim/Slim.php';
 
@@ -10,11 +11,30 @@ Slim\Slim::registerAutoloader();
 
 $app = new Slim\Slim();
 
+
+//Documentos
 $app->get('/tiposDocs', function(){
 	$tiposDocs = new tiposDocs();
 	$data = $tiposDocs->getAll();
 	sendResult($data);
 });
+
+
+//Duenios
+$app->post('/duenios', function(){
+    $request = Slim\Slim::getInstance()->request();
+    $data = json_decode($request->getBody(), true); //true convierte en array asoc, false en objeto php
+	
+	$duenio = new Duenio();
+    $result = $duenio->create($data);
+	
+	if($result){
+		sendResult($result);
+	}else{
+		sendError("Error al crear el producto");
+	}
+});
+
 
 
 $app->run();
--- api/modelos/duenios.php
+++ api/modelos/duenios.php
@@ -1,7 +1,7 @@
 <?php
 require_once("connection.php");
 
-class duenios
+class Duenio
 {
     private $connection;
     
@@ -9,17 +9,81 @@ class duenios
         $this->connection = Connection::getInstance();
     }
  
-   /* public function getAll(){
-        $query = "CALL SP_getTiposDoc();";
-        $tiposDocs= array();
+    public function create($duenio){
+        $usuario = $this->connection->real_escape_string($duenio['usuario']);
+        $contrasenia = $this->connection->real_escape_string($duenio['contrasenia']);
+        $nombre = $this->connection->real_escape_string($duenio['nombre']);
+        $apellido = $this->connection->real_escape_string($duenio['apellido']);
+        $idTipoDoc = $this->connection->real_escape_string($duenio['idTipoDoc']);
+        $nroDoc = $this->connection->real_escape_string($duenio['nroDoc']);
+        $email = $this->connection->real_escape_string($duenio['email']);
         
-        if( $result = $this->connection->query($query) ){
-            while($fila = $result->fetch_assoc()){
-                $tiposDocs[] = $fila;
-            }
-            $result->free();
+        $valor='';
+        
+        
+        // Parametros
+        $stmt = $c->prepare('SET @usuario := ?');
+        $stmt->bind_param('s', $usuario);
+        $stmt->execute();
+
+        $stmt = $c->prepare('SET @contrasenia := ?');
+        $stmt->bind_param('s', $contrasenia);
+        $stmt->execute();
+
+        $stmt = $c->prepare('SET @nombre := ?');
+        $stmt->bind_param('s', $nombre);
+        $stmt->execute();
+
+        $stmt = $c->prepare('SET @apellido := ?');
+        $stmt->bind_param('s', $apellido);
+        $stmt->execute();
+
+        $stmt = $c->prepare('SET @tipoDoc := ?');
+        $stmt->bind_param('i', $tipoDoc);
+        $stmt->execute();
+
+        $stmt = $c->prepare('SET @nroDoc := ?');
+        $stmt->bind_param('i', $nroDoc);
+        $stmt->execute();
+
+        $stmt = $c->prepare('SET @eMail := ?');
+        $stmt->bind_param('s', $eMail);
+        $stmt->execute();
+
+        //Salida
+        $stmt = $c->prepare('SET @valor := ?');
+        $stmt->bind_param('i', $valor);
+        $stmt->execute();
+        
+        
+        $query = "INSERT INTO productos VALUES (
+                    DEFAULT,
+                    '$idCategoria',
+                    '$descripcion',
+                    '$precio')";
+        if($this->connection->query($query)){
+            $producto['producto_id'] = $this->connection->insert_id;
+            return $producto;
+        }else{
+            return false;
         }
-        return $tiposDocs;
-    }*/
-   
+        
+        
+         // execute the stored Procedure
+        $result = $c->query('CALL SP_insertDuenios( @usuario, @contrasenia, @nombre, @apellido, @tipoDoc, @nroDoc,@eMail, @valor)');
+
+        // getting the value of the OUT parameter
+        $r = $c->query('SELECT @valor as duenio');
+        $row = $r->fetch_assoc();               
+
+        $res = $row['duenio'] ;
+
+        if($res > -1){
+            $dat= array( duenio  => $res);
+            sendResult($dat, 'OK' );
+        }else{
+           sendError("Error, no se pudo registrar al dueño."); //por algun motivo pincha aca
+        }
+        
+    }
 }
