b0a763333d7d939a8405e987c2a928491a40b367
pedro <iralapedro@gmail.com>
2016-03-31 23:57:37 -0300

    Localidades desde la base.

--- app/Common/services/LocalidadesService.js
+++ app/Common/services/LocalidadesService.js
@@ -1,12 +1,12 @@
 var app = angular.module("resergolApp");
 
-app.service("ProvinciasService", function($http, $resource){
+app.service("LocalidadesService", function($http, $resource){
 
 	//var PROVINCIAS_URI = "http://localhost/resergol1.1/api/provincias";
-    var PROVINCIAS_URI = "http://localhost:8080/resergol1.1/api/provincias";  //<PI>En mi casa es asi... 
+    var LOCALIDADES_URI = "http://localhost:8080/resergol1.1/api/localidades/:id";  //<PI>En mi casa es asi... 
     
     
-    return $resource(PROVINCIAS_URI, {}, {
+    return $resource(LOCALIDADES_URI, {}, {
         query: {method: 'GET',
                 transformResponse: function(data) {
                         return angular.fromJson(data).data;
@@ -15,4 +15,4 @@ app.service("ProvinciasService", function($http, $resource){
                 }
     });
 	
-});
+}); 
