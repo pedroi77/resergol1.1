57f4220230fca7acb42d6810d0b12df4a92618e3
Matias Fumacoo <matiasfumacoo@gmail.com>
2016-04-14 00:21:02 -0300

    arregle lo que se me rompio

---
+++ app/Admin/js/Admin.js
@@ -0,0 +1,16 @@
+var resergolApp = angular.module("resergolApp.Admin", [
+	"ui.router"
+]);
+
+resergolApp.config(function($stateProvider, $urlRouterProvider){
+//resergolApp.config(function($stateProvider){
+
+	$stateProvider
+		.state("Admin.administracion",{
+			url : "/admin/Administracion",
+			templateUrl : "app/Admin/Templates/AdminListaComplejos.html",
+			controllerAs : "admin"
+		});
+		$urlRouterProvider.otherwise("/admin");
+    
+}); 
