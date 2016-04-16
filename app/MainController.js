7dc5dcc5a15ea2012f368af4d69b0e64f3e8fd0a
pedro <iralapedro@gmail.com>
2016-04-16 15:27:48 -0300

    Fixs en templates (solo los mios)

--- app/MainController.js
+++ app/MainController.js
@@ -14,10 +14,12 @@ resergolApp.controller("MainController", function($state){
     this.validaLogin = function(){
          if(this.usuario.usuario == "dueño" && this.usuario.contrasenia == "dueño"){
              $state.go('Duenios');
+             $('#loginModal').modal('hide');
          };
        
         if(this.usuario.usuario == "admin" && this.usuario.contrasenia == "admin"){
              $state.go('Admin');
+             $('#loginModal').modal('hide');
         };
     };
 
